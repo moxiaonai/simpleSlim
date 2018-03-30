d0253f9910376f35154d72bb56b93dcb39951ce8
wuwenjing <wenjing.wu@deephi.tech>
2018-03-30 16:31:50 +0800

    'simpleSlim'

---
+++ src/public/lib/mysql.php
@@ -0,0 +1,10 @@
+<?php  
+  
+function connect_db() {  
+    $server = 'localhost'; // this may be an ip address instead  
+    $user = 'root';  
+    $pass = '';  
+    $database = 'test'; // name of your database  
+    $connection = new mysqli($server, $user, $pass, $database);  
+    return $connection;  
+}  
